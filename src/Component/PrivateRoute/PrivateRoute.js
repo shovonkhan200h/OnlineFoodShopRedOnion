eca2c8c1b43fd01a8f5d885931d635724c72662a
shovonkhan200h <shovonkhan00@gmail.com>
2024-03-24 22:52:52 +0000

    final version

--- src/Component/PrivateRoute/PrivateRoute.js
+++ src/Component/PrivateRoute/PrivateRoute.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Route, Navigate } from 'react-router-dom';
 
 export const PrivateRoute = ({ element, loggedIn, ...rest }) => {
-  return loggedIn ? (
+  console.log(loggedIn);
+  return loggedIn.isSingedIn ? (
     <Route {...rest} element={element} />
   ) : (
     <Navigate to="/Login" />
